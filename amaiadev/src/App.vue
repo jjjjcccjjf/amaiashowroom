71854a92d9731476c61ffdd02c9ae192f713e10f
treefunk <jlmarcelo@myoptimind.com>
2018-08-22 11:14:58 +0800

    Add amaia survey web

---
+++ amaiadev/src/App.vue
@@ -0,0 +1,101 @@
+<template>
+  <div id="app">
+    
+    <div id="mainapp" v-if="valid">
+      <Header />
+      <Breadcrumbs />
+      <section class="questionnaire">
+        <div class="pagewrapper2">
+            <QuestionCount />
+        <BuyingExperience />
+        <SiteVisitExperience />
+        <ShowroomSales />
+        <Product />
+        <HomeBuyingDecision />
+        <Agreement />
+        <PrevNext/>
+        </div>
+      </section>
+    </div>
+    <div v-else>
+      404
+    </div>
+  </div>
+</template>
+
+<script>
+
+import Header from './components/Header.vue'
+import Breadcrumbs from './components/Breadcrumbs.vue'
+import PrevNext from './components/PrevNext.vue'
+import QuestionCount from './components/QuestionCount.vue'
+
+//Questions
+import BuyingExperience from './components/BuyingExperience.vue'
+import SiteVisitExperience from './components/SiteVisitExperience.vue'
+import ShowroomSales from './components/ShowroomSales.vue'
+import Product from './components/Product.vue'
+import HomeBuyingDecision from './components/HomeBuyingDecision.vue'
+import Agreement from './components/Agreement.vue'
+//
+
+// import axios  from 'axios';
+import { mixin } from './mixins/mixin.js'
+import axios from 'axios';
+
+
+//Routes
+
+export default {
+  mounted(){
+  let params = window.location.search
+                .substring(1)
+                .split("&")
+                .map(v => v.split("="))
+                .reduce((map, [key, value]) => map.set(key, decodeURIComponent(value)), new Map())
+  let token = params.get('token')
+  axios({
+            method: 'GET',
+            url: `http://showroom.amaialand.com/api/token/check/?t=${token}`,
+            headers: {
+            "Content-Type": "application/json"
+            },
+            auth: {
+                username: 'admin',
+                password: '8NSaK!9=M}a({-18Nq9${vY-{#Z<WH7KDKhr(I*^yss|z{!<L[/"BNI@U(5?@#h'
+            },
+  }).then(response => {
+
+    if(Object.keys(response.data.personal_information.personal_information).length > 0){
+      this.$store.commit('setPersonalInfo',response.data.personal_information)
+      this.$store.commit('setMeta',response.data.meta)
+      this.valid = true
+    }
+  })
+
+  },
+  data(){
+    return {
+      valid: false
+    }
+  },
+  name: 'app',
+  mixins: [ mixin ],
+  components: {
+    QuestionCount,
+    Header,
+    Breadcrumbs,
+    PrevNext,
+    BuyingExperience,
+    SiteVisitExperience,
+    ShowroomSales,
+    Product,
+    HomeBuyingDecision,
+    Agreement
+  }
+}
+</script>
+
+<style>
+
+</style>
